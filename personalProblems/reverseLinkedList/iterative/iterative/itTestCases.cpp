37a02b1622bba5a1a36a50a43c1c45eff5aa7e1b
Mathew Clutter <mathew.clutter@mines.sdsmt.edu>
2021-01-05 19:55:00 -0700

    iterative linked list reversal

--- personalProblems/reverseLinkedList/iterative/iterative/itTestCases.cpp
+++ personalProblems/reverseLinkedList/iterative/iterative/itTestCases.cpp
@@ -13,12 +13,52 @@ TEST_CASE( "reverse" )
 
     l.insert( 5 );
     l.insert( 10 );
+    l.insert( 15 );
+    l.insert( 20 );
+    l.insert( 25 );
+    l.insert( 30 );
+    l.insert( 35 );
 
     l.itReverse();
 
     l.print( sout );
 
-    REQUIRE( sout.str() == "10, 5" );
+    REQUIRE( sout.str() == "35, 30, 25, 20, 15, 10, 5" );
+}
+
+
+TEST_CASE( "2 items" )
+{
+    stringstream sout;
+    sout.str( "" );
+
+    linkedList l2;
+
+    l2.insert( 1 );
+    l2.insert( 2 );
+
+    l2.itReverse();
+
+    l2.print( sout );
+
+    REQUIRE( sout.str() == "2, 1" );
+}
+
+
+TEST_CASE( "1 item" )
+{
+    stringstream sout;
+    sout.str( "" );
+
+    linkedList l1;
+
+    l1.insert( 5 );
+
+    l1.itReverse();
+
+    l1.print( sout );
+
+    REQUIRE( sout.str() == "5" );
 }
 
 /* int main()
--- personalProblems/reverseLinkedList/iterative/iterative/iterative.cpp
+++ personalProblems/reverseLinkedList/iterative/iterative/iterative.cpp
@@ -2,5 +2,21 @@
 
 void linkedList::itReverse()
 {
-    
+    node* prev;
+    node* curr;
+    node* next;
+
+    prev = nullptr;
+    curr = headptr;
+    next = nullptr;
+
+    while( curr != nullptr )
+    {
+        next = curr->next;
+        curr->next = prev;
+
+        prev = curr;
+        curr = next;
+    }
+    headptr = prev;
 }
